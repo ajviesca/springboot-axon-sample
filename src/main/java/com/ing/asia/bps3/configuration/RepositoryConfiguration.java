1d15c9d69e5229d54f733c6e2057dd8c3e493745
Adrian Joseph Viesca <Adrian.Joseph.Viesca@asia.ing.com>
2019-03-26 08:56:55 +0800

    add account repository bean config

--- src/main/java/com/ing/asia/bps3/configuration/RepositoryConfiguration.java
+++ src/main/java/com/ing/asia/bps3/configuration/RepositoryConfiguration.java
@@ -1,7 +1,10 @@
 package com.ing.asia.bps3.configuration;
 
+import com.ing.asia.bps3.core.entity.account.AccountRepository;
 import com.ing.asia.bps3.core.entity.biller.BillerRepository;
 import com.ing.asia.bps3.core.entity.payment.PaymentRepository;
+import com.ing.asia.bps3.infrastrcuture.domain.account.AccountJPA;
+import com.ing.asia.bps3.infrastrcuture.domain.account.AccountRepositoryImpl;
 import com.ing.asia.bps3.infrastrcuture.domain.biller.BillerJPA;
 import com.ing.asia.bps3.infrastrcuture.domain.biller.BillerRepositoryImpl;
 import com.ing.asia.bps3.infrastrcuture.domain.payment.PaymentJPA;
@@ -12,13 +15,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RepositoryConfiguration {
 
+    private final PaymentJPA paymentJPA;
+    private final BillerJPA billerJPA;
+    private final AccountJPA accountJPA;
+
+    public RepositoryConfiguration(PaymentJPA paymentJPA, BillerJPA billerJPA, AccountJPA accountJPA) {
+        this.paymentJPA = paymentJPA;
+        this.billerJPA = billerJPA;
+        this.accountJPA = accountJPA;
+    }
+
     @Bean
-    public BillerRepository billerRepository(BillerJPA billerJPA) {
+    public BillerRepository billerRepository() {
         return new BillerRepositoryImpl(billerJPA);
     }
 
     @Bean
-    public PaymentRepository paymentRepository(PaymentJPA paymentJPA) {
+    public PaymentRepository paymentRepository() {
         return new PaymentRepositoryImpl(paymentJPA);
+    }
+
+    @Bean
+    public AccountRepository accountRepository() {
+        return new AccountRepositoryImpl(accountJPA);
     }
 }
